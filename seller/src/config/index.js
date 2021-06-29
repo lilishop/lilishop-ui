7a080124c64fb6a0e803d87485aebd36ac162eb5
lemon橪 <17633066053@163.com>
2021-06-29 11:59:45 +0800

    新增会员提现功能

--- seller/src/config/index.js
+++ seller/src/config/index.js
@@ -22,10 +22,7 @@ export default {
     buyer: 'https://buyer-api.pickmall.cn',
     seller: 'https://store-api.pickmall.cn',
     manager: 'https://admin-api.pickmall.cn',
-    // common: 'http://192.168.0.100:8890',
-    // buyer: 'http://192.168.0.100:8888',
-    // seller: 'http://192.168.0.100:8889',
-    // manager: 'http://192.168.0.100:8887'
+
   },
   api_prod: {
     common: 'https://common-api.pickmall.cn',
