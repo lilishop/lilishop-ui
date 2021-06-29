9fbc61f7426f9284eb69667746fe910407f972d1
mabo <mailmabo@126.com>
2021-06-29 11:50:37 +0800

    拆分商品发布代码

---
+++ seller/src/views/goods/goods-seller/goodsOperationFirst.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+<style lang="scss" scoped>
+  @import "./addGoods.scss";
+</style>
