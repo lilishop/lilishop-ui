7b30bc42d7e2a2a6d1786c133fb4fba1e47af865
fengtianyangyang <tianyangyang.feng@javashop.cn>
2022-02-23 20:56:00 +0800

    添加

--- manager/public/config.js
+++ manager/public/config.js
@@ -3,10 +3,12 @@ var BASE = {
      * @description api请求基础路径
      */
     API_DEV: {
-      common: "https://common-api.pickmall.cn",
-      buyer: "https://buyer-api.pickmall.cn",
-      seller: "https://store-api.pickmall.cn",
-      manager: "https://admin-api.pickmall.cn"
+      // common: "https://common-api.pickmall.cn",
+      // buyer: "https://buyer-api.pickmall.cn",
+      // seller: "https://store-api.pickmall.cn",
+      // manager: "https://admin-api.pickmall.cn"
+      common: "http://127.0.0.1:8890",
+      manager: "http://127.0.0.1:8887"
     },
     API_PROD: {
       common: "https://common-api.pickmall.cn",
