25b403cde93dbf334435690d896ecd798dfb2bb7
fengtianyangyang <tianyangyang.feng@javashop.cn>
2022-02-26 15:05:51 +0800

    冲突文件

--- seller/public/config.js
+++ seller/public/config.js
@@ -3,19 +3,10 @@ var BASE = {
    * @description api请求基础路径
    */
   API_DEV: {
-<<<<<<< HEAD
-    // common: "https://common-api.pickmall.cn",
-    // buyer: "https://buyer-api.pickmall.cn",
-    // seller: "https://store-api.pickmall.cn",
-    // manager: "https://admin-api.pickmall.cn"
-    common: "http://127.0.0.1:8890",
-    seller: "http://127.0.0.1:8889",
-=======
     common: "http://localhost:8890",
     buyer: "http://localhost:8888",
     seller: "http://localhost:8889",
     manager: "http://localhost:8887"
->>>>>>> clerk
   },
   API_PROD: {
     common: "https://common-api.pickmall.cn",
