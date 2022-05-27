8a7f9b2487cf4fb9cb15e483812d17035278ec36
lemon橪 <17633066053@163.com>
2022-05-27 18:16:12 +0800

    批量商品价格展示

--- buyer/public/config.js
+++ buyer/public/config.js
@@ -3,8 +3,8 @@ var BASE = {
      * @description api请求基础路径
      */
     API_DEV: {
-      common: "https://common-api.pickmall.cn",
-      buyer: "https://buyer-api.pickmall.cn",
+      common: "http://192.168.0.106:8890",
+      buyer: "http://192.168.0.106:8888",
       seller: "https://store-api.pickmall.cn",
       manager: "https://admin-api.pickmall.cn"
     },
