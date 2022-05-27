160658cfb668ddecabd4ed9a37704ab955e4ed21
lemon橪 <17633066053@163.com>
2022-05-27 18:20:42 +0800

    批发商品部分

--- buyer/public/config.js
+++ buyer/public/config.js
@@ -3,8 +3,8 @@ var BASE = {
      * @description api请求基础路径
      */
     API_DEV: {
-      common: "http://192.168.0.106:8890",
-      buyer: "http://192.168.0.106:8888",
+      common: "https://common-api.pickmall.cn",
+      buyer: "https://buyer-api.pickmall.cn",
       seller: "https://store-api.pickmall.cn",
       manager: "https://admin-api.pickmall.cn"
     },
