bc45b168684a009386d1e6dbc88a5b3e9e337448
paulGao <paulgao0831@gmail.com>
2022-07-21 14:37:13 +0800

    update config api

--- manager/public/config.js
+++ manager/public/config.js
@@ -3,20 +3,20 @@ var BASE = {
      * @description api请求基础路径
      */
     API_DEV: {
-      common: "https://common-api.pickmall.cn",
-      buyer: "https://buyer-api.pickmall.cn",
-      seller: "https://store-api.pickmall.cn",
-      manager: "https://admin-api.pickmall.cn",
+      common: "http://101.43.247.178:30888",
+      buyer: "http://101.43.247.178:30888",
+      seller: "http://101.43.247.178:30888",
+      manager: "http://101.43.247.178:30888",
       //  manager: "http://192.168.0.120:8887",
       //  common: "http://192.168.0.120:8890",
 
 
     },
     API_PROD: {
-      common: "https://common-api.pickmall.cn",
-      buyer: "https://buyer-api.pickmall.cn",
-      seller: "https://store-api.pickmall.cn",
-      manager: "https://admin-api.pickmall.cn"
+      common: "http://101.43.247.178:30888",
+      buyer: "http://101.43.247.178:30888",
+      seller: "http://101.43.247.178:30888",
+      manager: "http://101.43.247.178:30888"
     },
     /**
      * @description // 跳转买家端地址 pc端
--- seller/public/config.js
+++ seller/public/config.js
@@ -3,16 +3,16 @@ var BASE = {
    * @description api请求基础路径
    */
   API_DEV: {
-    common: "https://common-api.pickmall.cn",
-    buyer: "https://buyer-api.pickmall.cn",
-    seller: "https://store-api.pickmall.cn",
-    manager: "https://admin-api.pickmall.cn"
+    common: "http://101.43.247.178:30888",
+    buyer: "http://101.43.247.178:30888",
+    seller: "http://101.43.247.178:30888",
+    manager: "http://101.43.247.178:30888"
   },
   API_PROD: {
-    common: "https://common-api.pickmall.cn",
-    buyer: "https://buyer-api.pickmall.cn",
-    seller: "https://store-api.pickmall.cn",
-    manager: "https://admin-api.pickmall.cn"
+    common: "http://101.43.247.178:30888",
+    buyer: "http://101.43.247.178:30888",
+    seller: "http://101.43.247.178:30888",
+    manager: "http://101.43.247.178:30888"
   },
   /**
    * @description // 跳转买家端地址 pc端
