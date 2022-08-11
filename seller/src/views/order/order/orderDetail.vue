283f4d6afa72a1585b319be368e17cd1082a9ed9
paulGao <paulgao0831@gmail.com>
2022-08-11 20:57:30 +0800

    修复订单列表查询订单类型查询无效问题

--- seller/src/views/order/order/orderDetail.vue
+++ seller/src/views/order/order/orderDetail.vue
@@ -409,14 +409,14 @@
         <dl>
           <dt>物流公司：</dt>
           <dd>
-            <div class="text-box">{{ logisticsInfo.shipper }}</div>
+            <div class="text-box">{{ logisticsInfo.shipper || orderInfo.order.logisticsName }}</div>
           </dd>
         </dl>
         <dl>
           <dt>快递单号：</dt>
           <dd>
             <div nctype="ordersSn" class="text-box">
-              {{ logisticsInfo.logisticCode }}
+              {{ logisticsInfo.logisticCode || orderInfo.order.logisticsNo }}
             </div>
           </dd>
         </dl>
