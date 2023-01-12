83f145767e46e285dd7dd7f1811ccb0639e09186
lemon橪 <17633066053@163.com>
2023-01-12 11:07:25 +0800

    refactor: 修改config API_DEV为线上掩饰站点api

--- seller/public/config.js
+++ seller/public/config.js
@@ -3,16 +3,16 @@ var BASE = {
    * @description api请求基础路径
    */
   API_DEV: {
-    common: "http://192.168.0.113:8890",
-    buyer: "http://192.168.0.113:8888",
-    seller: "http://192.168.0.113:8889",
-    manager: "http://192.168.0.113:8887"
+    common: "https://common-api.pickmall.cn",
+    buyer: "https://buyer-api.pickmall.cn",
+    seller: "https://store-api.pickmall.cn",
+    manager: "https://admin-api.pickmall.cn",
   },
   API_PROD: {
     common: "https://common-api.pickmall.cn",
     buyer: "https://buyer-api.pickmall.cn",
     seller: "https://store-api.pickmall.cn",
-    manager: "https://admin-api.pickmall.cn"
+    manager: "https://admin-api.pickmall.cn",
   },
   /**
    * @description // 跳转买家端地址 pc端
@@ -25,5 +25,5 @@ var BASE = {
   /**
    *  @description api请求基础路径前缀
    */
-  PREFIX: "/store"
+  PREFIX: "/store",
 };
