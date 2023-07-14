2c8e34008f59e3aed654ea6ce24504a8cadb89b5
Yer <17633066053@163.com>
2023-07-14 13:45:00 +0800

    轮播图 string类型改成 number类型 要不然有警告

--- buyer/src/components/indexDecorate/modelList/Carousel.vue
+++ buyer/src/components/indexDecorate/modelList/Carousel.vue
@@ -6,7 +6,7 @@
       <div class="nav-content">
         <!-- 轮播图 -->
 <!--        autoplay autoplay-speed="10000"-->
-        <Carousel :height="333.9" autoplay autoplay-speed="5000">
+        <Carousel :height="333.9" autoplay :autoplay-speed="5000">
           <CarouselItem  v-for="(item, index) in data.options.list" :key="index">
             <div class="swiper-img" >
               <img
