3bfa99f5785fe9b3ff19620d7095f1cf53312f7d
misworga831 <paulgao0831@gmail.com>
2024-06-14 11:30:53 +0800

    fix: 修复优惠券使用范围中的部分商品分类

--- seller/src/views/promotion/coupon/coupon-publish.vue
+++ seller/src/views/promotion/coupon/coupon-publish.vue
@@ -111,10 +111,7 @@
               <RadioGroup type="button" button-style="solid" v-model="form.scopeType">
                 <Radio :disabled="disabled" label="ALL">全品类</Radio>
                 <Radio :disabled="disabled" label="PORTION_GOODS">指定商品</Radio>
-                <!-- todo: 店铺端应为 部分店铺分类 后续补充 -->
-<!--                <Radio :disabled="disabled" label="PORTION_GOODS_CATEGORY"-->
-<!--                  >部分商品分类</Radio-->
-<!--                >-->
+                <Radio :disabled="disabled" label="PORTION_GOODS_CATEGORY">部分商品分类</Radio>
               </RadioGroup>
             </FormItem>
 
